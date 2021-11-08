66798d52ce7fedcc76eed34ec75e7eded96c1c2f
m-iida555 <user@usernoMacBook-Pro.local>
2021-11-08 19:20:15 +0900

    devise_profile_edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   
-  devise_for :users, controllers: {
-      registrations: 'users/registrations',
-      sessions: 'users/sessions'
-    }
+  devise_for :users
   
       
   root 'top#index'
